b077cea319163bb253677bcbdc76b36632b812b3
arso <arsobieraj@gmail.com>
2023-09-22 23:34:06 +0200

    pages test

---
+++ front/src/utils/channelValues.ts
@@ -0,0 +1,136 @@
+const MAIN_TYPE = {
+  ANALOG: 'Analog',
+  DJI: 'Dji',
+  HDZERO: 'Hdzero',
+  WS: 'Walksnail',
+} as const;
+
+type MainType = (typeof MAIN_TYPE)[keyof typeof MAIN_TYPE];
+
+type ChannelValue = {
+  id: number;
+  value: number;
+};
+
+type ChannelValues = {
+  id: string;
+  type: MainType;
+  values: ChannelValue[];
+};
+
+const djiO3LowWithout: ChannelValues = {
+  id: 'djiO3LowWithout',
+  type: MAIN_TYPE.DJI,
+  values: [
+    { id: 1, value: 5768 },
+    { id: 2, value: 5804 },
+    { id: 3, value: 5839 },
+  ],
+};
+
+const djiO3HighWithout: ChannelValues = {
+  id: 'djiO3HighWithout',
+  type: MAIN_TYPE.DJI,
+  values: [{ id: 1, value: 5794 }],
+};
+
+const djiO3LowHam: ChannelValues = {
+  id: 'djiO3LowHam',
+  type: MAIN_TYPE.DJI,
+  values: [
+    { id: 1, value: 5669 },
+    { id: 2, value: 5705 },
+    { id: 3, value: 5768 },
+    { id: 4, value: 5804 },
+    { id: 5, value: 5839 },
+    { id: 6, value: 5876 },
+    { id: 7, value: 5912 },
+  ],
+};
+
+const djiO3HighHam: ChannelValues = {
+  id: 'djiO3HighHam',
+  type: MAIN_TYPE.DJI,
+  values: [
+    { id: 1, value: 5677 },
+    { id: 2, value: 5794 },
+    { id: 3, value: 5902 },
+  ],
+};
+
+const djiVista: ChannelValues[] = [
+  {
+    id: 'djiVista25',
+    type: MAIN_TYPE.DJI,
+    values: [
+      { id: 1, value: 5660 },
+      { id: 2, value: 5695 },
+      { id: 3, value: 5735 },
+      { id: 4, value: 5770 },
+      { id: 5, value: 5805 },
+      { id: 8, value: 5839 },
+      { id: 6, value: 5878 },
+      { id: 7, value: 5914 },
+    ],
+  },
+  {
+    id: 'djiVista50',
+    type: MAIN_TYPE.DJI,
+    values: [
+      { id: 1, value: 5677 },
+      { id: 2, value: 5794 },
+      { id: 3, value: 5902 },
+    ],
+  },
+];
+
+const djiChannels: ChannelValues[] = [
+  djiO3LowWithout,
+  djiO3LowHam,
+  djiO3HighWithout,
+  djiO3HighHam,
+  ...djiVista,
+];
+
+const hdzeroChannels: ChannelValues[] = [
+  {
+    id: 'Hdzero',
+    type: MAIN_TYPE.HDZERO,
+    values: [
+      { id: 1, value: 5658 },
+      { id: 2, value: 5695 },
+      { id: 3, value: 5732 },
+      { id: 4, value: 5769 },
+      { id: 5, value: 5806 },
+      { id: 6, value: 5843 },
+      { id: 7, value: 5880 },
+      { id: 8, value: 5917 },
+    ],
+  },
+];
+
+const analogChannels: ChannelValues[] = [
+  {
+    id: 'Raceband',
+    type: MAIN_TYPE.ANALOG,
+    values: [
+      { id: 1, value: 5658 },
+      { id: 2, value: 5695 },
+      { id: 3, value: 5732 },
+      { id: 4, value: 5769 },
+      { id: 5, value: 5806 },
+      { id: 6, value: 5843 },
+      { id: 7, value: 5880 },
+      { id: 8, value: 5917 },
+    ],
+  },
+];
+
+export const wsChannels: ChannelValues[] = [];
+
+export const allChannels: ChannelValues[] = [
+  ...analogChannels,
+  ...hdzeroChannels,
+  ...djiChannels,
+  ...wsChannels,
+];
---
+++ front/src/views/ChannelsView.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="page">
+    <div class="systems">
+      <div
+        class="hazard"
+        v-for="line in hazardLines"
+        :key="line + Math.random()"
+        :style="{ left: (line - freqStart) / freqMul + '%' }"
+      >
+      </div>
+      <div
+        class="pickLine"
+        v-for="line in pickedLines"
+        :key="line"
+        :style="{ left: (line - freqStart) / freqMul + '%' }"
+      >
+        p
+      </div>
+      <div class="system" v-for="system in allChannels" :key="system.id">
+        {{ system.id }}
+        <div class="channels">
+          <template v-for="chObj in system.values" :key="chObj.id">
+            <button
+              :style="{ left: (chObj.value - freqStart) / freqMul + '%' }"
+              :class="{ 'channel--active': isPicked(system.id, chObj.id, chObj.value) }"
+              class="channel"
+              @click="toggleChannel(system.id, chObj.id, chObj.value)"
+            >
+              <div>{{ chObj.id }}</div>
+              <div>{{ chObj.value }}</div>
+            </button>
+          </template>
+        </div>
+      </div>
+    </div>
+    pickedChannels: {{ pickedChannels }}
+    <hr />
+    hazardLines: {{ hazardLines }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { allChannels } from '@/utils/channelValues';
+import {
+  computed, h, Ref, ref,
+} from 'vue';
+
+const freqStart = 5400;
+const freqEnd = 5950;
+const freqMul = (freqEnd - freqStart) / 100;
+
+const pickedChannels: Ref<string[]> = ref([]);
+
+const toggleChannel = (systemId: string, channelId: number, channelValue: number) => {
+  console.log(systemId, channelId);
+  const channnel = `${systemId}:${channelId}:${channelValue}`;
+  const index = pickedChannels.value.indexOf(channnel);
+  if (index !== -1) {
+    pickedChannels.value.splice(index, 1);
+  } else {
+    pickedChannels.value.push(channnel);
+  }
+};
+
+const isPicked = (systemId: string, channelId: number, channelValue: number) => {
+  const channnel = `${systemId}:${channelId}:${channelValue}`;
+  return pickedChannels.value.indexOf(channnel) !== -1;
+};
+
+const pickedLines = computed(() => pickedChannels.value.map((chObj) => chObj.split(':')[2]));
+// eslint-disable-next-line array-callback-return
+const hazardLines = computed(() => {
+  const elements = pickedLines.value;
+  const hazards = [];
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < elements.length; i++) {
+    // eslint-disable-next-line no-plusplus
+    for (let j = i + 1; j < elements.length; j++) {
+      hazards.push(elements[i] * 2 - elements[j]);
+      hazards.push(elements[j] * 2 - elements[i]);
+    }
+  }
+  return [...hazards];
+});
+</script>
+
+<style lang="scss">
+//.page {
+//  border: 1px solid red;
+//}
+
+.systems {
+  position: relative;
+  border: 3px solid black;
+}
+
+.system {
+  height: 90px;
+  border: 1px solid grey;
+}
+
+.channels {
+  position: relative;
+}
+
+.channel {
+  all: unset;
+  position: absolute;
+  border: 3px solid grey;
+  border-radius: 8px;
+  width: 6%;
+  height: 50px;
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  translate: -50%;
+  &:hover {
+    border: 3px solid red;
+  }
+  &--active {
+    background: #42b983;
+  }
+}
+
+.hazard {
+  position: absolute;
+  width: 4%;
+  height: 100%;
+  background: black;
+  opacity: 30%;
+  translate: -50%;
+}
+
+.pickLine {
+  position: absolute;
+  width: 2%;
+  height: 100%;
+  background: purple;
+  opacity: 30%;
+  translate: -50%;
+}
+</style>
